4f0259f23cd831f1f255ee474c7b950f76e51c16
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-12-04 18:49:20 -0500

    feat: add Google Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GOOGLE_ANALYTICS = process.env.GOOGLE_ANALYTICS || "";
+
   return (
     <html lang="es">
       <body className={inter.className}>
@@ -23,6 +26,7 @@ export default function RootLayout({
           {children}
         </main>
         <Toaster richColors />
+        <GoogleAnalytics gaId={GOOGLE_ANALYTICS} />
       </body>
     </html>
   );
